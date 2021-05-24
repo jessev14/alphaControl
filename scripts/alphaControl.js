c79799455af64aff8b845e9650e48894e17deeb6
jessev14 <jessevo321@gmail.com>
2021-05-23 21:58:28 -0400

    update for 0.8.x

--- scripts/alphaControl.js
+++ scripts/alphaControl.js
@@ -1,7 +1,7 @@
 Hooks.once("init", () => {
     game.settings.register("alphaControl", "hiddenToken", {
-        name: game.i18n.localize("alphaControl.settings.hiddenToken.name"),
-        hint: game.i19n.localize("alphaControl.settings.defaultHiddenHint"),
+        name: game.i18n.localize("alphaControl.settings.hiddenToken"),
+        hint: game.i18n.localize("alphaControl.settings.defaultHiddenHint"),
         scope: "world",
         config: true,
         type: Number,
@@ -14,8 +14,8 @@ Hooks.once("init", () => {
     });
 
     game.settings.register("alphaControl", "hiddenTile", {
-        name: game.i18n.localize("alphaControl.settings.hiddenTile.name"),
-        hint: game.i19n.localize("alphaControl.settings.defaultHiddenHint"),
+        name: game.i18n.localize("alphaControl.settings.hiddenTile"),
+        hint: game.i18n.localize("alphaControl.settings.defaultHiddenHint"),
         scope: "world",
         config: true,
         type: Number,
@@ -29,14 +29,14 @@ Hooks.once("init", () => {
 });
 
 const og_token_refresh = Token.prototype.refresh;
-Token.prototype.refresh = function () {
+CONFIG.Token.objectClass.prototype.refresh = function () {
   og_token_refresh.call(this);  
   this.icon.alpha = this.data.hidden ? game.settings.get("alphaControl", "hiddenToken") : this.data.alpha;
   return this;
 };
 
 const og_tile_refresh = Tile.prototype.refresh;
-Tile.prototype.refresh = function () {
+CONFIG.Tile.objectClass.prototype.refresh = function () {
     og_tile_refresh.call(this);
     this.tile.alpha = this.data.hidden ? game.settings.get("alphaControl", "hiddenTile") : this.data.alpha;
     return this;
